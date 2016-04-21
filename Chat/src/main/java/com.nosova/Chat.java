80058e8c1123f1ee9a57d7d865eaf3a4bee90a18
seamary <cr7ka@yandex.ru>
2016-04-21 11:18:22 +0300

    123

--- Chat/src/main/java/com.nosova/Chat.java
+++ Chat/src/main/java/com.nosova/Chat.java
@@ -3,49 +3,56 @@ package com.nosova;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.io.*;
 
 public class Chat extends HttpServlet {
-
-    private ArrayList<String> messages = new ArrayList<String>();
 
     @Override
     //метод возвращает список всех обращений
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            response.setContentType("text/html;charset=utf-8");
-
-            for (String message : messages) {
-                response.getWriter().write(message);
-            }
-            //читаю html из файла
-//            InputStream is = new FileInputStream(new File("src\\main\\resources\\form.txt"));
-//                Reader r = new InputStreamReader(is);
-//                BufferedReader br = new BufferedReader(r);
-//                while (true) {
-//                    String s = br.readLine();
-//                    if (s == null)
-//                        break;
-//                    response.getWriter().println(s);
-//                }
-//hghjghjgj
-            response.getWriter().println("<form.txt action=Chat.java method=\"post\">\n" +
-                    "<input type=\"text\" name=\"message\" size=40>\n" +
-                    "<input type=\"submit\" value=\"Отпарвить\">\n" +
-                    "</form.txt>");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+            responder(response);
     }
 
     @Override
     //отправляет строчку, обрабатывая запрос клиента
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter out = response.getWriter();
-        messages.add(request.getParameter("message"));
+        if (request != null) {
+            String message = request.getParameter("message");
+            String filePathDB = getServletContext().getRealPath("WEB-INF\\db.txt");
+            FileOutputStream os = new FileOutputStream(filePathDB, true);
+            os.write(message.getBytes(), 0, message.length());
+//            PrintStream ps = new PrintStream(fos);
+//            ps.println(request.getParameter("message"));
+            os.close();
+        }
+        responder(response);
     }
 
+    private void responder(HttpServletResponse response) {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            //читаю базу сообщений
+            txtReader(out, "WEB-INF\\db.txt");
+            //читаю html из файла
+            txtReader(out, "WEB-INF\\form.txt");
+        } catch (IOException e) {
+            out.print(e.getMessage());
+        }
+    }
+
+    private void txtReader(PrintWriter out, String path) throws IOException {
+        String filePath = getServletContext().getRealPath(path);
+        InputStream is = new FileInputStream(new File(filePath));
+        Reader r = new InputStreamReader(is, "utf-8");
+        BufferedReader br = new BufferedReader(r);
+        while (true) {
+            String s = br.readLine();
+            if (s == null)
+                break;
+            out.println(s);
+        }
+        is.close();
+    }
 }
